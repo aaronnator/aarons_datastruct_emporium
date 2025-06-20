d45bd560db431b8178efebfb0ba21fb951b5d6b8
Aaron <mbaaron@protonmail.com>
2025-06-20 01:28:50 -0400

    Added basic trie functionality.

--- menu.gd
+++ menu.gd
@@ -1,10 +1,13 @@
 extends Control
 
-
-
+#todo: standardize button_pressed method names
+var Trie = load("res://trie.gd")
+var trieObj = Trie.new()
 var LinkedList = load("res://LinkedList.gd")
 var list = LinkedList.new()
-@onready var displayLabel = $Panel/HBoxContainer/Panel/Display
+@onready var displayLabel = $Panel/HBoxContainer/LinkedList_Panel/Display
+@onready var text_box_submit = $Panel/HBoxContainer/Trie_Panel/TextEditTrieSubmit
+@onready var text_box_verify = $Panel/HBoxContainer/Trie_Panel/TextEditTrieVerify
 
 func _ready() -> void:
 	var list = LinkedList.new()
@@ -25,3 +28,19 @@ func _on_buttonAdd_pressed() -> void:
 func _on_button_clear_pressed() -> void:
 	list.delete_all()
 	displayLabel.text = ""
+
+
+func _on_button_submit_pressed() -> void:
+	var text = text_box_submit.text
+	print(text)
+	trieObj.insert(text)
+	
+	
+	
+
+
+func _on_button_verify_pressed() -> void:
+	var text = text_box_verify.text
+	if (trieObj.search(text)):
+		print("Trie confirmation.")
+		pass
--- trie.gd
+++ trie.gd
@@ -1,1 +1,47 @@
 #todo: trie
+
+extends Node
+
+class_name Trie
+
+# Trie Node definition
+class TrieNode:
+	var children := {}
+	var is_end_of_word := false
+
+	func _init():
+		children = {}
+		is_end_of_word = false
+
+# Trie main class
+var root := TrieNode.new()
+
+# Inserts a word into the trie.
+func insert(word: String) -> void:
+	var node = root
+	for i in word.length():
+		var char = word[i]
+		if not node.children.has(char):
+			node.children[char] = TrieNode.new()
+		node = node.children[char]
+	node.is_end_of_word = true
+
+# Returns true if the word is in the trie.
+func search(word: String) -> bool:
+	var node = root
+	for i in word.length():
+		var char = word[i]
+		if not node.children.has(char):
+			return false
+		node = node.children[char]
+	return node.is_end_of_word
+
+# Returns true if there is any word in the trie that starts with the given prefix.
+func starts_with(prefix: String) -> bool:
+	var node = root
+	for i in prefix.length():
+		var char = prefix[i]
+		if not node.children.has(char):
+			return false
+		node = node.children[char]
+	return true
